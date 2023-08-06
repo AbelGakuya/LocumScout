11e0b4664359f288f980220cc36a701ee4b5e366
AMG <abelgakuya@gmail.com>
2023-08-06 14:39:32 +0300

    Added Navigation graph and implemented the dark theme

--- app/src/main/java/com/locums/locumscout/MainActivity.kt
+++ app/src/main/java/com/locums/locumscout/MainActivity.kt
@@ -2,10 +2,24 @@ package com.locums.locumscout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        setSupportActionBar(toolbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        bottomNavigationView.setOnNavigationItemReselectedListener { /* No Operation*/ }
     }
 }
