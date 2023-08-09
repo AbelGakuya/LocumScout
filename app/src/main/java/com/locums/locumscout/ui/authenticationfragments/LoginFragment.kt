0238d5e1f8682c4af4032c1b6efca8791e350f34
AMG <abelgakuya@gmail.com>
2023-08-09 18:17:54 +0300

    Added Log in funtion

--- app/src/main/java/com/locums/locumscout/ui/authenticationfragments/LoginFragment.kt
+++ app/src/main/java/com/locums/locumscout/ui/authenticationfragments/LoginFragment.kt
@@ -5,10 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.locums.locumscout.R
+import com.locums.locumscout.databinding.FragmentLoginBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 
 class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+    lateinit var auth: FirebaseAuth
 
 
     override fun onCreateView(
@@ -16,7 +27,50 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnLogin.setOnClickListener {
+            loginUser()
+        }
+
+        return view
     }
+
+    override fun onStart() {
+        super.onStart()
+        checkLoggedInState()
+    }
+
+    private fun loginUser(){
+        val email = binding.edtEmail.text.toString()
+        val password = binding.edtPassword.text.toString()
+        if (email.isNotEmpty() && password.isNotEmpty()){
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.signInWithEmailAndPassword(email, password).await()
+                    withContext(Dispatchers.Main){
+                        checkLoggedInState()
+                    }
+
+                } catch (e:Exception){
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkLoggedInState(){
+        if (auth.currentUser == null){
+            Toast.makeText(requireContext(), "You are not logged in", Toast.LENGTH_SHORT).show()
+        } else {
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+    }
+
 
 }
