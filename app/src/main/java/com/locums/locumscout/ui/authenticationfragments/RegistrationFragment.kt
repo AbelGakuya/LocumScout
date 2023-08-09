dd1c10e10bdc5124b61d320186265f378395b030
AMG <abelgakuya@gmail.com>
2023-08-09 18:10:20 +0300

    Added Firebase and registration code

--- app/src/main/java/com/locums/locumscout/ui/authenticationfragments/RegistrationFragment.kt
+++ app/src/main/java/com/locums/locumscout/ui/authenticationfragments/RegistrationFragment.kt
@@ -5,14 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.locums.locumscout.R
 import com.locums.locumscout.databinding.FragmentRegistrationBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 
 class RegistrationFragment : Fragment() {
 
     private lateinit var binding: FragmentRegistrationBinding
+
+    lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,14 +33,45 @@ class RegistrationFragment : Fragment() {
         binding = FragmentRegistrationBinding.inflate(inflater,container, false)
         val view = binding.root
 
+        auth = FirebaseAuth.getInstance()
+
         binding.btnRegister.setOnClickListener {
 
-            findNavController().navigate(R.id.action_registrationFragment_to_homeFragment)
+            registerUser()
+            //findNavController().navigate(R.id.action_registrationFragment_to_homeFragment)
         }
 
         return view
     }
 
+    private fun registerUser(){
+        val email = binding.edtEmail.text.toString()
+        val password = binding.edtPassword.text.toString()
+        if (email.isNotEmpty() && password.isNotEmpty()){
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.createUserWithEmailAndPassword(email, password).await()
+                    withContext(Dispatchers.Main){
+                        checkLoggedInState()
+                    }
+
+                } catch (e:Exception){
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun checkLoggedInState(){
+        if (auth.currentUser == null){
+            Toast.makeText(requireContext(), "You are not logged in", Toast.LENGTH_SHORT).show()
+        } else {
+            findNavController().navigate(R.id.action_registrationFragment_to_homeFragment)
+        }
+    }
 
 
 }
